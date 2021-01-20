cab6cb814ff4448e62aca2bace477b2ec76a1122
as30098 <alexspencer421@gmail.com>
2021-01-20 11:26:04 -0500

    final push

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ let ramenMenu = document.querySelector('#ramen-menu')
 let ramenDetail = document.querySelector('#ramen-detail')
 let ramenRatingForm = document.querySelector('#ramen-rating')
 
+
 // fetch all of the data in the database currently and populate images
-fetch('http://localhost:3000/ramens')
+let showAllRamen = () => {fetch('http://localhost:3000/ramens')
 .then(res => res.json())
 .then((ramenObj) =>
   ramenObj.forEach(ramen =>
     turnRamenIntoImg(ramen)
-  ));
+  ))
+};
 
 // turning url into img in div
 let turnRamenIntoImg = (ramen) => {
@@ -24,53 +26,55 @@ let turnRamenIntoImg = (ramen) => {
 
 }
 
-// click individual ramen - event listener
-ramenMenu.addEventListener('click', function(e) {
-  e.preventDefault()
-  // target image
-  let ramenImg = document.querySelector('.detail-image')
-  //update
-  ramenImg.src.innerHTML = e.target.outerHTML
-  console.log(e.target.outerHTML)
+showAllRamen()
 
-  // //target name
-  // let ramenName = document.querySelector('.name')
-  // //update
-  // ramenName.innerText =
+// // click individual ramen - event listener
+// ramenMenu.addEventListener('click', function(e) {
+//   e.preventDefault()
+//   // target image
+//   let ramenImg = document.querySelector('.detail-image')
+//   //update
+//   ramenImg.src.innerHTML = e.target.outerHTML
+//   console.log(e.target.outerHTML)
 
-  // //target restaurant
-  // let ramenRestaurant = document.querySelector('.restaurant')
-  // //update
-  // ramenRestaurant.innerText =
-})
+//   //target name
+//   let ramenName = document.querySelector('.name')
+//   //update
+//   ramenName.innerText =
 
-// update rating and comment - event listener
-ramenRatingForm.addEventListener('submit', ratingAndComment)
+//   //target restaurant
+//   let ramenRestaurant = document.querySelector('.restaurant')
+//   //update
+//   ramenRestaurant.innerText =
+// })
 
-let ratingAndComment = (evt) => {
-  // evt.preventDefault() is needed to avoid refresh
-   evt.preventDefault()
-  // still slightly shaky on how to target object id
-  fetch(`http://localhost:3000/ramens/${ramen.id}`, {
-    method: "PATCH",
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    // the data is the user input...needs to be transformed to fit database
-    // need to create the data variable still...
-    body: JSON.stringify(data),
-  })
-  .then(res => res.json())
-  .then(data => {
-    // update rating
-    // update comment
-    // persist to the database
-    // update the DOM with new info
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  })
-}
+// // update rating and comment - event listener
+// ramenRatingForm.addEventListener('submit', ratingAndComment)
+
+// let ratingAndComment = (evt) => {
+//   // evt.preventDefault() is needed to avoid refresh
+//    evt.preventDefault()
+//   // still slightly shaky on how to target object id
+//   fetch(`http://localhost:3000/ramens/${ramen.id}`, {
+//     method: "PATCH",
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     // the data is the user input...needs to be transformed to fit database
+//     // need to create the data variable still...
+//     body: JSON.stringify(data),
+//   })
+//   .then(res => res.json())
+//   .then(data => {
+//     // update rating
+//     // update comment
+//     // persist to the database
+//     // update the DOM with new info
+//   })
+//   .catch((error) => {
+//     console.error('Error:', error);
+//   })
+// }
 
 
 
