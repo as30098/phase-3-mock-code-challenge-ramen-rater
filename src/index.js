dc025f4b5ae59fc4547d7185ef146c4c9db3a74c
as30098 <alexspencer421@gmail.com>
2021-01-20 10:05:04 -0500

    first deliverable completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-// write your code here
+// stable elements at the top
+let ramenMenu = document.querySelector('#ramen-menu')
+
+// fetch all of the data in the database currently
+
+fetch('http://localhost:3000/ramens')
+.then(res => res.json())
+.then((ramenObj) =>
+  ramenObj.forEach(ramen =>
+    turnRamenIntoImg(ramen)
+  ));
+
+
+// unstable element creation
+let turnRamenIntoImg = (ramen) => {
+
+  // artisanal way of creating elements
+  // element creation
+  let ramenImg = document.createElement('img')
+  // attribute creation
+  ramenImg.src = ramen.image
+  // relationship creation
+  ramenMenu.append(ramenImg)
+
+
+}
+
+// click individual ramen - event listener
+ramenMenu.addEventListener('click', function() {
+
+})
+
+
+
+  // query on outer element
+
+  // create event listeners for newly created elements
+
+  // update rating and comment - event listener
+    // evt.preventDefault() is needed to avoid refresh
+
+
+
+
+
+
+
